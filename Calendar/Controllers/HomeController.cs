fcf9f8be533436ee9e705f64b2b8d09086dc2d8e
snehaprasai <sweto.sneha@gmail.com>
2017-09-25 06:41:03 +0545

    inital commit

---
+++ Calendar/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Calendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new EventViewModel());
+        }
+
+        public JsonResult GetEvents(DateTime start, DateTime end)
+        {
+            var viewModel = new EventViewModel();
+            var events = new List<EventViewModel>();
+            start = DateTime.Today.AddDays(-14);
+            end = DateTime.Today.AddDays(-11);
+
+            for (var i = 1; i <= 5; i++)
+            {
+                events.Add(new EventViewModel()
+                {
+                    id = i,
+                    title = "Event " + i,
+                    start = start.ToString(),
+                    end = end.ToString(),
+                    allDay = false
+                });
+
+                start = start.AddDays(7);
+                end = end.AddDays(7);
+            }
+
+
+            return Json(events.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+    }
+    }
